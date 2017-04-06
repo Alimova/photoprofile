a4e5d8e0a9840358012f1981aa01c46c4f1cf408
Anna Alimova <new@alimov.net>
2017-04-06 12:37:05 +0300

    Edit/Delete work!

--- src/app/edit-listing/edit-listing.component.ts
+++ src/app/edit-listing/edit-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-listing',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-listing.component.css']
 })
 export class EditListingComponent implements OnInit {
+  id;
+  title;
+  owner;
+  city;
+  bedrooms;
+  price;
+  image;
+  type;
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getListingDetails(this.id).subscribe(listing =>{
+      this.title = listing.title;
+      this.city = listing.city;
+      this.type = listing.type;
+      this.owner = listing.owner;
+      this.bedrooms = listing.bedrooms;
+      this.price = listing.price;
+    })
+  }
+
+  onEditSubmit(){
+    let listing = {
+      title: this.title,
+      city: this.city,
+      owner: this.owner,
+      type: this.type,
+      price:this.price,
+      bedrooms: this.bedrooms
+    }
+
+    this.firebaseService.updateListing(this.id, listing);
+
+    this.router.navigate(['/listings']);
   }
 
 }
